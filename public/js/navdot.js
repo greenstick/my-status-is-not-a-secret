83a784e7e16fc74186a2748fe16d3d78c71f4c33
Ben Cordier <bcordier@columnfivemedia.com>
2014-04-01 12:49:55 -0700

    Version 0.5.1
    Minor copy edits

--- public/js/navdot.js
+++ public/js/navdot.js
@@ -6,30 +6,29 @@ var Dot = function (element, home) {
 	var dot = this;
 		dot.element = $(element),
 		dot.home = $(home);
-}
+};
 
 //Gets Link Position
 Dot.prototype.linkPosition = function (link) {
 	var link = link.data();
 		return link.position;
-}
+};
 //Gets Default Dot Position
 Dot.prototype.defaultPosition = function () {
 	var home = this.home.data();
 		return home.position;
-}
+};
 //Animates Dot To Hovered Link
 Dot.prototype.linkHover = function (link) {
 	var to = this.linkPosition(link);
 		this.element.stop().animate({"left": to}, 400);
 		return to;
-}
+};
 //Animates Dot to Home Position
 Dot.prototype.linkOut = function () {
 	var to = this.defaultPosition();
 		this.element.stop().animate({"left": to}, 300);
-		return to;
-}
+};
 //Initializes Default Dot Position
 Dot.prototype.init = function () {
 	var def = this.defaultPosition();
@@ -46,8 +45,8 @@ var dot = new Dot('#dot', '.homePosition');
 
 $('.nav a, .goHome').on("mouseover", function (e) {
 	dot.linkHover($(this));
-})
+});
 
 $('.nav a, .goHome').on("mouseout", function (e) {
 	dot.linkOut();
-})
+});
